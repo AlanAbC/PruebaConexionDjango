d07071edc1cbed9bf81995e90062400317720d9e
alana <alanjiraya77@gmail.com>
2017-10-03 22:41:27 -0500

    comit

---
+++ URL.java
@@ -0,0 +1,13 @@
+package com.claresti.pruebaconexiondjango;
+
+/**
+ * Created by alana on 03/10/2017.
+ */
+
+public class URL {
+    private String urlPregunta;
+    public Urls() {
+        this.urlPregunta = "http://dg.iwsandbox.com:8000/onboard/api/set_answer/";
+
+    }
+}
